d980cbdfd40ba0c50af6d12a2d3eb0ef8680fcc1
Soujanya C. Aryal <67591327+soujanya957@users.noreply.github.com>
2022-09-11 22:37:23 +0545

    updates

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+    var logo = document.querySelector(".logodiv");
+    logo.classList.toggle("scrolling", window.scrollY > 0);
+});
+
+let dayNight = document.querySelector('.dayNight')
+let body = document.querySelector('body')
+let moon = document.querySelector('.t1')
+let lime = document.querySelector('.t2')
+
+dayNight.onclick = function() {
+    body.classList.toggle('dark')
+    dayNight.classList.toggle('active')
+    moon.classList.toggle('active')
+    lime.classList.toggle('active')
+}
+
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        padding: 200
+    });
+    autoplay();
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
+});
